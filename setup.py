0605f32db4140e19796091c38e499050b1bda248
shneidmiller <v.shneidmiller@gmail.com>
2024-06-18 11:55:45 +0100

    fix action and setup

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="robotframework-testrail-sync",
-    version="0.0.2",
+    version="0.0.3",
     description="A tool to synchronize Robot Framework tests with TestRail",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author="Viacheslav Shneidmillier",
     author_email="v.shneidmiller@gmail.com",
     url="https://github.com/yourusername/robotestrail",
-    packages=find_packages(),
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     include_package_data=True,
     install_requires=[
         "requests",
@@ -23,7 +24,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'robotestrail=robotestrail.src.main:main',
+            'robotestrail=main:main',
         ],
     },
     python_requires='>=3.6',
